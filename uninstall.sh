3e965aba4a18ac703c2a7ddfc0c6af2788079eae
bryan kennedy <bkennedy@smm.org>
2014-12-04 10:36:00 -0600

    Adding temp files to the uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -3,5 +3,7 @@
 rm -rf ~/Library/LaunchAgents/org.smm.*
 rm -rf ~/Desktop/sd-waw
 rm -rf ~/Desktop/c2c
-
-
+rm -rf /tmp/c2c-err.log
+rm -rf /tmp/c2c-report.log
+rm -rf /tmp/meteor-err.log
+rm -rf /tmp/meteor-report.log
